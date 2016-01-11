2054001b60638907935680c372fe2d18eb6f17cb
Ray Cothern <rtcothern@gmail.com>
2016-01-11 08:25:29 -0800

    Create structure class

---
+++ Source/Moba/Structure.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Moba.h"
+#include "Structure.h"
+
+AStructure::AStructure() {
+
+}
+
+// Called when the game starts or when spawned
+void AStructure::BeginPlay() {
+
+}
+
+// Called every frame
+void AStructure::Tick(float DeltaSeconds) {
+		
+}
---
+++ Source/Moba/Structure.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Interactable.h"
+#include "Structure.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOBA_API AStructure : public AInteractable
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets default values for this character's properties
+	AStructure();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+};
